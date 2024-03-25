4eefc9074bdb200c1193d2eab056ba202a1220af
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-25 11:27:19 +0900

    feat(NavbarComponent): complete navbar html, styles, and typescript.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  @Output() toggleDarkMode = new EventEmitter<void>();
 
+  onToggleDarkMode(): void {
+    this.toggleDarkMode.emit();
+  }
 }
