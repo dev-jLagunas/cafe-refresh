a9716738dcc6c9dcd38c2790f61c1721e84bf629
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-26 08:14:02 +0900

    chore: configure flowbite.

--- src/app/landing-home/landing-home.component.ts
+++ src/app/landing-home/landing-home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroLandingComponent } from './hero-landing/hero-landing.component';
 import { AboutLandingComponent } from './about-landing/about-landing.component';
+import { MenuLandingComponent } from './menu-landing/menu-landing.component';
 
 @Component({
   selector: 'app-landing-home',
   standalone: true,
-  imports: [HeroLandingComponent, AboutLandingComponent],
+  imports: [HeroLandingComponent, AboutLandingComponent, MenuLandingComponent],
   templateUrl: './landing-home.component.html',
   styleUrl: './landing-home.component.scss',
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   darkMode: "class",
   theme: {
     extend: {
@@ -26,5 +26,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
