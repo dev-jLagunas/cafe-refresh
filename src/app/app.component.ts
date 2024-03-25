7ff1b8cca33b316efd7ca654f62c912ce2a3694c
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-25 11:24:13 +0900

    feat(index.html): write up index html head.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'cafe-refresh';
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+  toggleDarkMode() {
+    const body = this.document.body;
+    const hasDarkClass = body.classList.contains('dark');
+
+    if (hasDarkClass) {
+      this.renderer.removeClass(body, 'dark');
+    } else {
+      this.renderer.addClass(body, 'dark');
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { LandingHomeComponent } from './landing-home/landing-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: LandingHomeComponent }];
