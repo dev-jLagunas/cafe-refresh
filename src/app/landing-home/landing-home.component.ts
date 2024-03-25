b18d17c4c6f33d8b4e5093f71229e8c1f2231d45
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-25 11:29:35 +0900

    feat(LandingHomeComponent): add child components to parent landinghome component.

--- src/app/landing-home/landing-home.component.ts
+++ src/app/landing-home/landing-home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroLandingComponent } from './hero-landing/hero-landing.component';
+import { AboutLandingComponent } from './about-landing/about-landing.component';
 
 @Component({
   selector: 'app-landing-home',
   standalone: true,
-  imports: [],
+  imports: [HeroLandingComponent, AboutLandingComponent],
   templateUrl: './landing-home.component.html',
-  styleUrl: './landing-home.component.scss'
+  styleUrl: './landing-home.component.scss',
 })
-export class LandingHomeComponent {
-
-}
+export class LandingHomeComponent {}
