c0c4489fac0aa6b37c158b78c3d7034c1d3e7870
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-25 11:13:00 +0900

    chore: set up initial tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      display: ["group-hover"],
+      width: {
+        "90vw": "90vw",
+      },
+      height: {
+        "90vh": "90vh",
+      },
+      screens: {
+        xs: "425px",
+      },
+      fontFamily: {
+        comfortaa: ["Comfortaa", "sans-serif"],
+      },
+      colors: {
+        "bg-light": "#FFF7FC",
+        "bg-dark": "#191919",
+      },
+      fontSize: {
+        xxs: "11px",
+      },
+    },
+  },
+  plugins: [],
+};
