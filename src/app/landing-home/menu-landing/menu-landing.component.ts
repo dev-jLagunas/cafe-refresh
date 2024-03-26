7fdd563e8d1af6631c6262c5c24a5ecfd6fb7475
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-26 10:37:04 +0900

    feat(MenuLanding): complete menu landing component html, styles, and typescript logic.

--- src/app/landing-home/menu-landing/menu-landing.component.ts
+++ src/app/landing-home/menu-landing/menu-landing.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-menu-landing',
   standalone: true,
   imports: [],
   templateUrl: './menu-landing.component.html',
-  styleUrl: './menu-landing.component.scss'
+  styleUrl: './menu-landing.component.scss',
 })
-export class MenuLandingComponent {
-
+export class MenuLandingComponent implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
