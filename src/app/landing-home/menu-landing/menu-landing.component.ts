100aedff9fe20b4a0cd125c01a9a0d6aacf6b148
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-02 09:53:01 +0900

    feat(MenuLanding): translate Menu Landing component text context to dynamically populate in English, Spanish, Japanese using ngx-translate.

--- src/app/landing-home/menu-landing/menu-landing.component.ts
+++ src/app/landing-home/menu-landing/menu-landing.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-menu-landing',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TranslateModule],
   templateUrl: './menu-landing.component.html',
   styleUrl: './menu-landing.component.scss',
 })
