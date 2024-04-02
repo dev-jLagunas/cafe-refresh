0d098feeeab3ea9c7515210c53d5705f14da4372
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-02 10:03:37 +0900

    feat(GalleryLanding): create and populate text content translations dynamically using ngx-translate.

--- src/app/landing-home/gallery-landing/gallery-landing.component.ts
+++ src/app/landing-home/gallery-landing/gallery-landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-gallery-landing',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TranslateModule],
   templateUrl: './gallery-landing.component.html',
   styleUrl: './gallery-landing.component.scss',
 })
