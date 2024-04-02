2b4d8a6ca7ef7b7a1fbae91b50d23c8c4a103f65
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-02 10:25:21 +0900

    feat(GalleryLanding): create and populate ngx-translate dynamically translations for Gallery Landing component text content.

--- src/app/landing-home/info-landing/info-landing.component.ts
+++ src/app/landing-home/info-landing/info-landing.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-info-landing',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './info-landing.component.html',
-  styleUrl: './info-landing.component.scss'
+  styleUrl: './info-landing.component.scss',
 })
-export class InfoLandingComponent {
-
-}
+export class InfoLandingComponent {}
